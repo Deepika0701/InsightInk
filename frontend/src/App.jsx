3d9a669fbbf9239ea6aff9516b222e6f46b0749d
Deepika <82407112+Deepika0701@users.noreply.github.com>
2024-06-11 14:55:39 +0530

    frontend1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./Pages/Home";
+import { Routes,Route } from "react-router-dom";
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <div className='mt-8 w-full bg-black px-8 md:px-[500px] flex justify-between text-sm md:text-md py-8'>
+      <div className='flex flex-col text-white '>
+        <p>Featured blogs</p>
+        <p>Most viewed</p>
+        <p>Readers choice</p>
+      </div>
+      <div className='flex flex-col text-white '>
+        <p>Forum</p>
+        <p>Supports</p>
+        <p>Recent Posts</p>
+      </div>
+      <div className='flex flex-col text-white '>
+        <p>Privacy Policy</p>
+        <p>About Us</p>
+        <p>Terms & Conditions</p>
+        <p>Terms of Services</p>
+      </div>
+    </div>
+    <p className='py-2 pb-2 text-center text-white bg-black'>
+      All rights reserved &copy; Insight Ink
+    </p>
+    </>
+
+  )
+}
+
+export default Footer
